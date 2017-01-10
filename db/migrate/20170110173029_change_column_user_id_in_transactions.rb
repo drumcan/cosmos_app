578b885bb934a07c5486309129553e1ff94e095e
Dan O'Connor <daniel.oconnor@getbraintree.com>
2017-01-10 11:40:53 -0600

    changed integer to 'integer USING CAST(column_name AS integer)'

--- db/migrate/20170110173029_change_column_user_id_in_transactions.rb
+++ db/migrate/20170110173029_change_column_user_id_in_transactions.rb
@@ -1,6 +1,6 @@
 class ChangeColumnUserIdInTransactions < ActiveRecord::Migration
   def up
-    change_column :transactions, :user_id, :integer
+    change_column :transactions, :user_id, 'integer USING CAST(column_name AS integer)'
   end
 
   def down
